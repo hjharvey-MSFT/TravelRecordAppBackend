0cc27568f78e4f01a433ea92730399a4991cab78
hjharvey-MSFT <57494070+hjharvey-MSFT@users.noreply.github.com>
2020-04-27 10:18:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 var app = require('express')(); // Create an instance of an Express app
 
-var mobileApp = require('azure-mobile-apps')
-(
-    {
-    // Explicitly enable the Azure Mobile Apps home page
-    homePage: true,
-    // Explicitly enable swagger support. UI support is enabled by
-    // installing the swagger-ui npm module.
-    // swagger: true,
-    // App will use MS_SqliteFilename or MS_TableConnectionString to choose the SQLite or SQL data provider
-    data: {
-        dynamicSchema: true
-        }
-    }
-); // Create an instance of a Mobile App with default settings
+var mobileApp = require('azure-mobile-apps')();
 
 
 mobileApp.tables.add('Post'); // Add a table for 'Post' with default settings
